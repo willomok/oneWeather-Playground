d838711763ae5f471ef45a9fe55079644d14d184
Will Omok <willlook7@gmail.com>
2024-08-03 12:48:19 +0100

    sorted css into a styles folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './app.css'
+import './styles/app.css'
+import './styles/weather.css'
+import './styles/nav.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
